f75a97e2659d0a27a6bbda12853adb620ce00152
muqeet007 <muqeetnabila308@gmail.com>
2025-06-22 00:34:59 +0500

    Implement TaskProvider context and integrate TaskList and TaskItem components; update styles in index.css

--- src/components/TaskProvider.jsx
+++ src/components/TaskProvider.jsx
@@ -3,7 +3,15 @@ import { TaskContext } from "../context/TaskContext";
 
 const TaskProvider=({children})=>
 {
-    const [task,setTask]=useState({})
+    const [task,setTask]=useState(
+        {
+            id: '334',     // Unique task ID (Date.now() or UUID)
+            title: 'sccc',           // Task title
+            notes: 'string',           // Optional notes/details
+            completed: 'boolean',      // Done or not
+            createdAt: 'string',       // Timestamp (ISO or readable format)
+            updatedAt: 'string'        // Updated timestamp
+    })
     return(
         <TaskContext.Provider value={{task,setTask}}>
             {children}
---
+++ src/components/Taskitem.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+
+
+const TaskItem=()=>
+{   
+    const {task}=useContext(TaskContext)
+        return(
+        <>
+           
+        </>
+    );
+}
+
+
+export default TaskItem;
